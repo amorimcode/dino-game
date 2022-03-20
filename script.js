4f382a62b93a43e39f7689c61b1a95200b2fd4d7
amorimcode <bruno.amorim032@gmail.com>
2022-03-20 13:30:45 -0300

    update

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 const dino = document.getElementById("dino");
 const cactus = document.getElementById("cactus");
 
+// Conta os segundos para marcar pontos
+let segundos = 0;
+function ContarSegundos() {
+    document.getElementById('secs').innerHTML = segundos
+    segundos++
+}
+setInterval(ContarSegundos, 1000);
+
 function jump() {
     if (dino.classList != "jump") {
         dino.classList.add("jump");
 
-        setTimeout(function () {
+        // depois do time out, tira a classe jump do dino
+        setTimeout(() => {
             dino.classList.remove("jump");
         }, 300);
     }
 }
+var counter = 0;
 
 let isAlive = setInterval(() => {
-    let randomTime = Math.random() * 6000; //criar cactus de maneira aleatória
+    let randomTime = Math.random() * 1000;
 
-    // dino posição
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
 
-    // cactus posição
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-    // analisar se ambos se tocam
-    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
-        alert("Vixe, game over :(");
-        document.body.innerHTML = '<h1 class="game-over""> Atualize a página e jogue novamente </h1>'; // criando título ao final do jogo
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+        alert('Perdeu') 
+        segundos = 0
     }
 
     setTimeout(isAlive, randomTime);
 }, 10);
 
-document.addEventListener("keydown", function (event) {
+document.addEventListener("keydown", event => {
     jump();
+    counter++
 });
